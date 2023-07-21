016e5998940ee521b370cbcda3c2622f026aa22f
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-22 02:43:05 +0300

    feat: add routes & links to them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MoviesComponent } from './components/movies/movies.component';
+import { SeriesComponent } from './components/series/series.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+const routes: Routes = [
+  { path: 'movies', component: MoviesComponent },
+  { path: 'series', component: SeriesComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
