99af2fc4c675437b0ac05f18b26dff57c1a045c7
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-21 21:00:53 +0300

    feat: disable logo link on root

--- src/app/components/ui/logo/logo.component.ts
+++ src/app/components/ui/logo/logo.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss']
+  styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent {
+export class LogoComponent implements OnInit {
+  isRoot = false;
 
+  constructor(private location: Location, private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      if (this.location.path() !== '') {
+        this.isRoot = false;
+      } else {
+        this.isRoot = true;
+      }
+    });
+  }
 }
