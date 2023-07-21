ba9390bb6d791f7b68e0a396386cdc3541ea1114
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-21 22:11:16 +0300

    refactor: logo now just image, remove link
    link will be added later if need it

--- src/app/components/ui/logo/logo.component.ts
+++ src/app/components/ui/logo/logo.component.ts
@@ -9,18 +9,4 @@ import { Router } from '@angular/router';
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
 })
-export class LogoComponent implements OnInit {
-  isRoot = false;
-
-  constructor(private location: Location, private router: Router) {}
-
-  ngOnInit() {
-    this.router.events.subscribe(() => {
-      if (this.location.path() !== '') {
-        this.isRoot = false;
-      } else {
-        this.isRoot = true;
-      }
-    });
-  }
-}
+export class LogoComponent {}
