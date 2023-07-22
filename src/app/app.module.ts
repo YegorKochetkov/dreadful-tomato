fd587b5e5a7c8be111ade75d5ba4dd464355565d
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-22 11:26:19 +0300

    feat: move nav links to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { LogoComponent } from './components/ui/logo/logo.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LogoComponent],
+  imports: [BrowserModule, AppRoutingModule, LogoComponent, MainMenuComponent],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [AppRoutingModule],
 })
 export class AppModule {}
