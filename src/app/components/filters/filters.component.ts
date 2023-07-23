6893c71c230f08deddd724ec63aababef9009229
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-23 17:26:22 +0300

    feat: show filter panel by click filters button

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.scss']
+  styleUrls: ['./filters.component.scss'],
 })
 export class FiltersComponent {
-
+  @Input() isShown = false;
 }
