a40f02897c58f81e27fc8698b0cc7d75a33cdb0a
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-23 23:02:44 +0300

    feat: implement filters form

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss'],
 })
 export class FiltersComponent {
   @Input() isShown = false;
+
+  filtersForm = new FormGroup({
+    searchFilter: new FormControl('', [Validators.required]),
+    yearFilter: new FormControl(''),
+  });
+
+  log() {
+    console.log(this.filtersForm.value.yearFilter);
+  }
 }
