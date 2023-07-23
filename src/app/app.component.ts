5dbe0de689a5fdf130ead53a176b0a0d291b0959
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-23 11:20:37 +0300

    refactor: update layout & styles after changing components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dreadful Tomato';
+  isRoot = false;
+
+  constructor(private location: Location, private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.isRoot = this.location.path() === '';
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
-import { LogoComponent } from './components/ui/logo/logo.component';
+import { AuthMenuComponent } from './components/(header)/auth-menu/auth-menu.component';
+import { HeaderLogoComponent } from './components/(header)/header-logo/header-logo.component';
+import { MainMenuComponent } from './components/(header)/main-menu/main-menu.component';
+import { NavbarFiltersComponent } from './components/(header)/navbar-filters/navbar-filters.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LogoComponent, MainMenuComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MainMenuComponent,
+    AuthMenuComponent,
+    NavbarFiltersComponent,
+    HeaderLogoComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [AppRoutingModule],
