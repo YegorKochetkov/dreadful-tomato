f796d67a1e46c956962c5498ac981ac016d57745
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-23 17:40:12 +0300

    feat: hide menu, filters button & panel on root & auth paths

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -24,6 +24,7 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
 })
 export class HeaderComponent implements OnInit {
   isRoot = false;
+  isAuth = false;
   isFiltersDisplay = false;
 
   constructor(private location: Location, private router: Router) {}
@@ -31,6 +32,9 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.router.events.subscribe(() => {
       this.isRoot = this.location.path() === '';
+      this.isAuth =
+        this.location.path() === '/sign-in' ||
+        this.location.path() === '/sign-up';
     });
   }
 
