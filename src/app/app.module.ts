fdedc7ee1da2e2cff064ad86312892976f1e796b
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-25 14:49:12 +0300

    build: install @angular/material  add custom palette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
   exports: [AppRoutingModule],
