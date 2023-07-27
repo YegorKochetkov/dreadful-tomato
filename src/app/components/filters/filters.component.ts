26e49dcaebf25cd22906cb406375235920b1dd18
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-27 23:52:18 +0300

    feat: replace angular material date picker by primeng calendar - more lightweight & customizable lib

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,15 +1,58 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { YearPickerComponent } from '../ui/year-picker/year-picker.component';
-import { SearchFilterComponent } from '../ui/search-filter/search-filter.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { SearchFilterComponent } from '../search-filter/search-filter.component';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [CommonModule, YearPickerComponent, SearchFilterComponent],
+  imports: [
+    CommonModule,
+    CalendarModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    SearchFilterComponent,
+  ],
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss'],
 })
 export class FiltersComponent {
   @Input() isShown = false;
+  isCalendarOpened = false;
+
+  filtersForm = new FormGroup({
+    searchFilter: new FormControl(''),
+    yearFilter: new FormControl<Date | null>(null),
+  });
+
+  focusOnInput(input: HTMLInputElement) {
+    input.focus();
+  }
+
+  getClassOf() {
+    if (this.isCalendarOpened) {
+      return 'calendar-opened';
+    }
+    return '';
+  }
+
+  setIsCalendarOpened(value: boolean) {
+    this.isCalendarOpened = value;
+  }
+  // TODO: add query to url (on submit only?)
+  log1() {
+    console.log('on form submit', this.filtersForm.value);
+  }
+  log2() {
+    console.log('on form change', this.filtersForm.value);
+  }
+  log4() {
+    console.log('on close', this.filtersForm.controls.yearFilter.value);
+  }
+  log5() {
+    console.log('on clear', this.filtersForm.controls.yearFilter.value);
+  }
 }
