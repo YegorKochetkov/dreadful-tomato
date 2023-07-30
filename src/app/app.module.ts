97e760dcca5bc938a803e0fb7b834277d423f604
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-07-30 23:16:29 +0300

    feat: add http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
@@ -12,8 +13,9 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderComponent,
     BrowserAnimationsModule,
+    HttpClientModule,
+    HeaderComponent,
     FooterComponent,
   ],
   providers: [],
